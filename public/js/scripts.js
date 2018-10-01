4656c72345da206ce0078b9cc2d006cebeafe23c
Gray Smith <gray.smith00@gmail.com>
2018-10-01 15:48:46 -0600

    Add jQuery cdn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,9 @@
-alert('hiii there!');
+window.onload = function() {
+  if (window.jQuery) {
+    // jQuery is loaded
+    alert('Yeah!');
+  } else {
+    // jQuery is not loaded
+    alert("Doesn't Work");
+  }
+};
