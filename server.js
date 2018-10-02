ea9022f37c059b2ce4be1d1bec4711f15c88c867
Gray Smith <gray.smith00@gmail.com>
2018-10-02 15:14:37 -0600

    Add cors and bodyParser setup in server.js file

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+// Cors
+app.use(
+  cors({
+    allowedOrigins: ['localhost:3000']
+  })
+);
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
+
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
 const projects = require('./routes/projects');
 app.use('/api/v1/projects', projects);
 
+// app listening
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
   console.log(`App listening on port ${app.get('port')}`);
