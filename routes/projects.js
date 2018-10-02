a099c209557e2604690687bafbe1ede94c72ad25
Gray Smith <gray.smith00@gmail.com>
2018-10-02 15:13:56 -0600

    Remove projects prefix from projects routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,42 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world');
-});
-
 // get all projects
-router.get('/projects', (req, res) => {
+router.get('/', (req, res) => {
   res.send('projects');
 });
 
 // project post request
-router.post('/projects', (req, res) => {
-  res.send('projects post request');
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('worked');
 });
 
 // get individual project
-router.get('/projects/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   res.send(`project id: ${req.params.id}`);
 });
 
 // get all palettes in a project
-router.get('/projects/:id/palettes', (req, res) => {
+router.get('/:id/palettes', (req, res) => {
   res.send('get all palettes');
 });
 
 // post new palette to project
-router.post('/projects/:id/palettes', (req, res) => {
+router.post('/:id/palettes', (req, res) => {
   res.send('palette post request');
 });
 
 // get individual palette
-router.get('/projects/:id/palettes/:id', (req, res) => {
+router.get('/:id/palettes/:id', (req, res) => {
   res.send('get individual palette');
 });
 
 // delete palette
-router.delete('/projects/:id/palettes/:palette_id', (req, res) => {
+router.delete('/:id/palettes/:palette_id', (req, res) => {
   res.send('delete palette');
 });
 
